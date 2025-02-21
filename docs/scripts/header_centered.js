ee4ed11e386a2a45e9ec1b7bc72cd181991303d2
King Nothing <zacharyh1199@gmail.com>
2025-02-20 22:26:26 -0600

    doc update

--- docs/scripts/header_centered.js
+++ docs/scripts/header_centered.js
@@ -8,9 +8,3 @@ var logo = document.createElement('a');
 logo.setAttribute('href', './index.html');
 document.body.appendChild(logo);
 logo.innerHTML = '<div id="logo" style="text-align: center; margin-left: auto; margin-right: auto;"><img src="./images/logo.png" style="border: solid 1px #000;"></img></div>';
-
-/*
-var bubble = document.createElement('script');
-bubble.type = "text/javascript";
-bubble.src = "scripts/dontuse.js?323425";
-document.body.appendChild(bubble);*/

33077b72095aa8bd9dc3ead70cfc7e9ec7faf44f
King Nothing <zacharyh1199@gmail.com>
2025-04-27 20:48:02 -0500

    Docs update

--- docs/scripts/updates.js
+++ docs/scripts/updates.js
@@ -2,7 +2,7 @@ var rows = document.getElementsByTagName('tr');
 for(var i = 0; i < rows.length; i++)
 {
 	if(i % 2 != 0)
-		rows[i].style.backgroundColor = "#333333";
+	rows[i].style.backgroundColor = "#333333";
 	else
-		rows[i].style.backgroundColor = "#444444";
+	rows[i].style.backgroundColor = "#444444";
 }

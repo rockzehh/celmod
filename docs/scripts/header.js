75867f3a32fa3247e2c53368012b5e456afb3a22
King Nothing <zacharyh1199@gmail.com>
2025-04-27 19:26:13 -0500

    Updated webscripts

--- docs/scripts/header.js
+++ docs/scripts/header.js
@@ -6,6 +6,6 @@ document.body.appendChild(favicon);
 
 var bg_logo = 'url(https://raw.githubusercontent.com/rockzehh/celmod/refs/heads/main/docs/images/logo.png) 0 0 repeat-y';
 var logo = document.createElement('a');
-logo.setAttribute('href', 'celmod.rockzehh.net/index.html');
+logo.setAttribute('href', 'http://celmod.rockzehh.net/');
 document.body.appendChild(logo);
 logo.innerHTML = '<img src="https://raw.githubusercontent.com/rockzehh/celmod/refs/heads/main/docs/images/logo.png" style="border: solid 1px #000;"></img>';
